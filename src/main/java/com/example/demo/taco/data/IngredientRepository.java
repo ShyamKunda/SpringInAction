9e67c1e5666c03974c62f264d240db6afe87bc71
shyamkunda <shyamk.iiit@gmail.com>
2020-02-22 13:35:38 +0530

    Added Ingredient repository with JDBC Template

---
+++ src/main/java/com/example/demo/taco/data/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.taco.data;
+
+import com.example.demo.taco.model.Ingredient;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+    Ingredient findOne(String id);
+    Ingredient save(Ingredient ingredient);
+
+}
